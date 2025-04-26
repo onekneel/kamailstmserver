37e5c88783ea86bfaa1583ea9a102c796667ceeb
Johnniel Mar <marjohnniel@gmail.com>
2025-04-26 08:26:10 +0800

    hh

--- app.py
+++ app.py
@@ -222,4 +222,4 @@ def predict():
         return jsonify({"error": "Invalid file type. Allowed: mp4, avi, mov"}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
